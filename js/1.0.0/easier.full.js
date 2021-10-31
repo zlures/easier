91608a85997aef1299785981e65d03533c204d30
Vlada <81084648+zlures@users.noreply.github.com>
2021-10-31 10:57:01 +0300

    Update easier.full.js

--- js/1.0.0/easier.full.js
+++ js/1.0.0/easier.full.js
@@ -1,20 +1,11 @@
-'use strict';
-
-/**
- * Construct a new Easier instance by passing the configuration object
- *
- * @param {String | HTMLElement} element
- * @param {Object} opts
- */
-
-var Cards = document.getElementById("easier-card");
-/* Cards */
-if (Cards === "null") {
-return null;
-}
-else {
-var Input = document.getElementById("easier-card-input").value
-if (Input.startsWith("4") === true) {
-/* Visa */
-} 
-} 
+var Easier=Easier||(function () {
+   var privateArray=[];
+   var privateFunction=function(){
+   }
+   return {
+      show: function(input){
+         privateFunction();
+         alert(input);
+      }
+   }
+})();
